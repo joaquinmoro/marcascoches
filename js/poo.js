7540e3fa9ed8179099bf2e28b29f1fa69f9446df
joaquinmoro <contacto@joaquinmoro.com>
2020-05-30 12:35:17 +0200

    Creada la estructura y el contenido de la web en html, definido el estilo en CSS y creado un objeto en js sin saber cómo integrarlo en el html

---
+++ js/poo.js
@@ -0,0 +1,13 @@
+// CREACIÓN DE UN OBJETO CON 3 COCHES
+
+var coches = {
+    imagen: // ¿PUEDEN AÑADIRSE IMÁGENES A UN OBJETO?
+    marca: ["FERRARI", "FIAT", "BMW"],
+    color: ["Rojo", "Rojo", "Gris"],
+    anyo: [2015, 2018, 2016]
+}
+
+// "INTENTO" DE LLAMAR UNA PROPIEDAD PARA INSERTARLO EN EL HTML
+
+
+coches.marca[1] = document.getElementsByClassName('cocheUno_marca');
